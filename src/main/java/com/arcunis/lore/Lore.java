4b7cb27aa91a8b898cd1c3c8b8b7a97cc8d991dd
phant0m2290 <titulaerkay@gmail.com>
2024-10-14 09:41:22 +0200

    Initial commit

---
+++ src/main/java/com/arcunis/lore/Lore.java
@@ -0,0 +1,23 @@
+package com.arcunis.lore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lore extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        // Plugin load logic
+
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
