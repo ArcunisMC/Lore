e6caaaffbdc971936a0bd8acb26e829a465626c8
phant0m2290 <titulaerkay@gmail.com>
2024-10-20 17:34:40 +0200

    Remove unused imports

--- src/main/java/com/arcunis/lore/custom/enchantments/ExampleEnchantment.java
+++ src/main/java/com/arcunis/lore/custom/enchantments/ExampleEnchantment.java
@@ -1,24 +1,12 @@
 package com.arcunis.lore.custom.enchantments;
 
 import com.arcunis.lore.custom.Enchantment;
-import io.papermc.paper.registry.RegistryKey;
-import io.papermc.paper.registry.TypedKey;
 import io.papermc.paper.registry.data.EnchantmentRegistryEntry;
 import io.papermc.paper.registry.event.RegistryFreezeEvent;
-import io.papermc.paper.registry.keys.ItemTypeKeys;
-import io.papermc.paper.registry.keys.tags.ItemTypeTagKeys;
-import io.papermc.paper.registry.set.RegistryKeySet;
-import io.papermc.paper.registry.set.RegistrySet;
-import io.papermc.paper.registry.tag.TagKey;
-import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Registry;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemType;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
