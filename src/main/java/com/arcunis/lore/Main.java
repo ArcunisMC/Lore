dfa0b05a515ae5a66b2a8758850e5a5c0ab5b38f
phant0m2290 <titulaerkay@gmail.com>
2024-10-20 15:55:04 +0200

    Register enchantment events in Main

--- src/main/java/com/arcunis/lore/Main.java
+++ src/main/java/com/arcunis/lore/Main.java
@@ -1,9 +1,11 @@
 package com.arcunis.lore;
 
 import com.arcunis.lore.commands.GiveCustomCommand;
+import com.arcunis.lore.custom.Enchantment;
 import com.arcunis.lore.custom.Item;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -13,6 +15,11 @@ public final class Main extends JavaPlugin {
         // Register custom items
         for (Item<?> item : Bootstrapper.registry.getAllItems()) {
             item.register();
+        }
+
+        // Register custom enchantment events
+        for (Enchantment enchantment : Bootstrapper.registry.getAllEnchantments()) {
+            Bukkit.getPluginManager().registerEvents(enchantment, this);
         }
 
         // Register events
